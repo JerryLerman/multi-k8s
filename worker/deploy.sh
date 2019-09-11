951d8165b848e6f013c9b5b18360cc359c07ef10
Jerry Lerman <lerman@runbox.com>
2019-09-11 19:54:46 -0400

    Moved dev only k8s file to separate folder

--- worker/deploy.sh
+++ worker/deploy.sh
@@ -1,8 +1,16 @@
-docker build -t jerrylerman/multi-client -f ./client/Dockerfile ./client
-docker build -t jerrylerman/multi-server -f ./server/Dockerfile ./server
-docker build -t jerrylerman/multi-worker -f ./worker/Dockerfile ./worker
-docker push jerrylerman/multi-client
-docker push jerrylerman/multi-server
-docker push jerrylerman/multi-worker
+docker build -t jerrylerman/multi-client:latest -t jerrylerman/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jerrylerman/multi-server:latest -t jerrylerman/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jerrylerman/multi-worker:latest -t  jerrylerman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jerrylerman/multi-client:latest
+docker push jerrylerman/multi-server:latest
+docker push jerrylerman/multi-worker:latest
+
+docker push jerrylerman/multi-client:$SHA
+docker push jerrylerman/multi-server:$SHA
+docker push jerrylerman/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jerrylerman/multi-server
+kubectl set image deployments/server-deployment server=jerrylerman/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jerrylerman/multi-client:$SHA
+kubect set  image deployments/worker-deployment worker=jerrylerman/multi-worker:$SHA
