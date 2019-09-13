c3b5e7dbbc691095b792a36d995a5b4b8e1a61d2
Jerry Lerman <lerman@runbox.com>
2019-09-12 20:25:07 -0400

    Fixed deploy.sh mistake

--- worker/deploy.sh
+++ worker/deploy.sh
@@ -1,6 +1,6 @@
 docker build -t jerrylerman/multi-client:latest -t jerrylerman/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jerrylerman/multi-server:latest -t jerrylerman/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jerrylerman/multi-worker:latest -t  jerrylerman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jerrylerman/multi-worker:latest -t jerrylerman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push jerrylerman/multi-client:latest
 docker push jerrylerman/multi-server:latest
@@ -13,4 +13,4 @@ docker push jerrylerman/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jerrylerman/multi-server:$SHA
 kubectl set image deployments/client-deployment client=jerrylerman/multi-client:$SHA
-kubect set  image deployments/worker-deployment worker=jerrylerman/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=jerrylerman/multi-worker:$SHA
